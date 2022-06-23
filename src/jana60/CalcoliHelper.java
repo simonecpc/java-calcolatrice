bf1f9d0feb87c8379c6a75b1baaf391af349e1dc
simonecpc <simone101199@gmail.com>
2022-06-23 16:56:57 +0200

    bonus 1

--- src/jana60/CalcoliHelper.java
+++ src/jana60/CalcoliHelper.java
@@ -36,10 +36,43 @@ public class CalcoliHelper {
 	public static int massimo(int a, int b){
 		if(b>a) {
 			return b;}
-		else {
+		else { 
 			return a;}
+	}
+		// Bonus 1 utilizzando metodo overload cambiando int in double
 		
+		public static double somma(double a, double b){
+			return a+b;
+		}
 		
+		public static double differenza(double a, double b){
+			return a-b;
+		}
+		
+		public static double moltiplicazione(double a, double b){
+			return a*b;
+		}
+		
+		public static double vAssoluto (double a){
+			if (a<0) {
+				return -a;}
+			else {
+				return a;}
+			
+		}
+		
+		public static double minimo(double a, double b){
+			if(b<a) {
+				return b;}
+			else {
+				return a;}
+			}
+		
+		public static double massimo(double a, double b){
+			if(b>a) {
+				return b;}
+			else {
+				return a;}
 		
 	}
 }
--- src/jana60/Main.java
+++ src/jana60/Main.java
@@ -4,19 +4,36 @@ public class Main {
 
 	public static void main(String[] args) {
 		
+//		System.out.println("Operazioni con i valori interi:");
+//		
+//		System.out.println(CalcoliHelper.somma(6, 8));
+//		
+//		System.out.println(CalcoliHelper.differenza(9, 18));
+//		
+//		System.out.println(CalcoliHelper.moltiplicazione(14, 3));
+//		
+//		System.out.println(CalcoliHelper.vAssoluto(-6));
+//		
+//		System.out.println(CalcoliHelper.minimo(18, 5));
+//		
+//		System.out.println(CalcoliHelper.massimo(15, 2));
+//	
+		// INIZIO BONUS 1
 		
+		System.out.println("Operazioni con i valori double:");
 		
-		System.out.println(CalcoliHelper.somma(6, 8));
+		System.out.println(CalcoliHelper.somma(5.3, 6.4));
 		
-		System.out.println(CalcoliHelper.differenza(9, 18));
+		System.out.println(CalcoliHelper.differenza(2.2, 6.3));
 		
-		System.out.println(CalcoliHelper.moltiplicazione(14, 3));
+		System.out.println(CalcoliHelper.moltiplicazione(1.0, 3.5));
 		
-		System.out.println(CalcoliHelper.vAssoluto(-6));
+		System.out.println(CalcoliHelper.vAssoluto(-55.55));
 		
-		System.out.println(CalcoliHelper.minimo(18, 5));
+		System.out.println(CalcoliHelper.minimo(90.5, 31.2));
 		
-		System.out.println(CalcoliHelper.massimo(15, 2));
+		System.out.println(CalcoliHelper.massimo(47.4, 10.3));
+		
 	}
 	
 }
